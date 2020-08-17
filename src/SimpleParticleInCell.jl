4787bbf1fb8ff520b33cbb8a8833927c1a6c9d2e
Dominik Stańczak <stanczakdominik@gmail.com>
2020-08-17 14:31:50 +0200

    Cell span constructor

--- src/SimpleParticleInCell.jl
+++ src/SimpleParticleInCell.jl
@@ -2,9 +2,23 @@ module SimpleParticleInCell
     using StaticArrays
 
     struct World
-        mesh_origin :: SVector{3}
-        cell_spacing :: SVector{3}
+        mesh_origin :: SVector{3, Float64}
+        cell_spacing :: SVector{3, Float64}
         number_cells ::SVector{3, Int}
+        testint :: Int
+        function World(
+                       r1 :: Vector,
+                       r2 :: Vector,
+                       number_cells :: Vector{Int},
+                      )
+            cell_spacing = (r2 .- r1) ./ number_cells
+            new(
+                r1,
+                cell_spacing,
+                number_cells
+               )
+        end
+
     end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,10 @@ using SimpleParticleInCell
 using Test
 
 @testset "SimpleParticleInCell.jl" begin
-    world = SimpleParticleInCell.World([0,0,0], [1,1,1], [4,4,4])
+    world = SimpleParticleInCell.World([0.0,0.0,0.0],
+                                       [1.0,1.0,1.0], 
+                                       [4,4,4],
+                                       )
     @test world.mesh_origin == [0,0,0]
-    @test world.cell_spacing == [1., 1., 1.]
+    @test world.cell_spacing == [0.25, 0.25, 0.25]
 end
